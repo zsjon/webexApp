bb4cfc3dc24113ff888cd6887bf3b1bd2930dc6d
zsj <cho010105@naver.com>
2025-03-28 13:37:18 +0900

    Updates

---
+++ webex-embedded-app-sdk.js
@@ -0,0 +1,12 @@
+
+// Placeholder for Webex Embedded App SDK
+// Original: https://binaries.webex.com/static-content-widget/webex-embedded-app/webex-embedded-app-sdk.js
+
+(function(window) {
+  window.Webex = {
+    EmbeddedAppSdk: function() {
+      this.ready = () => Promise.resolve();
+      this.getUser = () => Promise.resolve({ email: "test@example.com" });
+    }
+  };
+})(window);
